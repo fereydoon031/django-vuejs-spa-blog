0550ae6195cd16b6a6193662f06c05111b410678
Fereydoon <ferydoon.jafari@gmail.com>
2022-07-06 15:44:27 +0430

    feat: limit access to views

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Store from '../store'
 import HomeView    from '../views/HomeView.vue'
 import AboutView   from '../views/AboutView.vue'
 import ProfileView from '../views/ProfileView.vue'
@@ -19,17 +20,21 @@ const routes = [
   {
     path: '/profile',
     name: 'profile',
-    component: ProfileView
+    component: ProfileView,
+    meta: { requiresAuth: true }
   },
   {
     path: '/login',
     name: 'login',
-    component: LoginView
+    component: LoginView,
+    meta: { requiresRedirect: true }
   },
   {
     path: '/logout',
     name: 'logout',
-    component: LogoutView
+    component: LogoutView,
+    meta: { requiresAuth: true }
+
   },
 ]
 
@@ -37,5 +42,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!Store.state.isAuthenticated) {
+      next('/login')
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.requiresRedirect)){
+    if (Store.state.isAuthenticated) {
+      next('/profile')
+    } else {
+      next()
+    }
+  }
+  else {
+    next() // make sure to always call next()!
+  }
+})
 export default router
