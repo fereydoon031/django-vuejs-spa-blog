00b873bddb7d1fa6eeaa586d08599c6c1c96d7f6
Fereydoon <ferydoon.jafari@gmail.com>
2022-07-06 13:34:23 +0430

    feat: Start with store for login and logout

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,34 @@
 <template>
-<nav  id="nav" class="navbar navbar-expand-lg bg-light rounded" aria-label="Eleventh navbar example">
-      <div class="container-fluid">
-        <router-link class="navbar-brand"  to="/" >Blog</router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExample09">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/about">About</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/profile">Profile</router-link>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown09" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-              <ul class="dropdown-menu" aria-labelledby="dropdown09">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-          </ul>
-          <form role="search">
-            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-          </form>
-        </div>
-      </div>
-    </nav>
+<NavBar/>
+{{$store.state}}
 
+<br/>
+
+{{$route}}
   <router-view class="container"/>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'AppView',
+  components: {
+    NavBar
+  },
+  watch:{
+    $route(){
+    this.$store.commit("onStart")
+    }
+  },
+  mounted(){
+    this.$store.commit("onStart")
+  }
+}
+</script>
+
 
 <style>
 #app {
@@ -43,21 +37,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-.navbar-brand {
-    color: black !important;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+<nav  id="nav" class="navbar navbar-expand-lg bg-light rounded" aria-label="Eleventh navbar example">
+      <div class="container-fluid">
+        <router-link class="navbar-brand"  to="/" >Blog</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample09">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/profile">Profile</router-link>
+            </li>
+          </ul>
+          <form role="search">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+          </form>
+        </div>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+.navbar-brand {
+    color: black !important;
+}
+</style>
