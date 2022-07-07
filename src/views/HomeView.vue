fc610e35681eb809da3a38ba77522e13f0c21d18
Fereydoon <ferydoon.jafari@gmail.com>
2022-07-07 12:46:25 +0430

    feat: Add articles to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,87 @@
 <template>
   <div class="home text-left">
     <h1> Home Page</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero ea rem voluptates,
-      amet inventore nesciunt velit eligendi aliquid ducimus consequuntur adipisci aliquam corporis,
-      voluptatem expedita veniam exercitationem libero voluptatibus ex.
-      </p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero ea rem voluptates,
-       amet inventore nesciunt velit eligendi aliquid ducimus consequuntur adipisci aliquam corporis,
-       voluptatem expedita veniam exercitationem libero voluptatibus ex.
-      </p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero ea rem voluptates,
-      amet inventore nesciunt velit eligendi aliquid ducimus consequuntur adipisci aliquam corporis,
-      voluptatem expedita veniam exercitationem libero voluptatibus ex.
-      </p>
+    <article v-for="article in articles" :key="article.id">
+    
+    <h2 >{{article.title}}</h2>
+    <p>
+      {{article.abstract}}
+
+    </p>
+<hr>
+    </article>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  data() {
+    
+    return {
+      articles : [{
+      id: '1',
+      title: 'title 1',
+      slug: 'title-1',
+      abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.",
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.'
+    },
+    {
+      id: '2',
+      title: 'title 2',
+      slug: 'title-2',
+      abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.",
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.'
+    },
+    {
+      id: '3',
+      title: 'title 3',
+      slug: 'title-3',
+      abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.",
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.\n\
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quibusdam \n\
+                 laboriosam perferendis officia sequi praesentium illum, ut in suscipit aut \n\
+                 consequuntur, neque omnis. Qui nemo eaque exercitationem quibusdam.'
+    }]
+    }
+  },
 }
+
 </script>
