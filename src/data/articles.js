5f41948f66ca474d953cc99aacfe45c22f54cdc9
Fereydoon <ferydoon.jafari@gmail.com>
2022-07-10 15:36:21 +0430

    data: Add article data as part of the project - avoid the local storage

---
+++ src/data/articles.js
@@ -0,0 +1,94 @@
+
+export const articlesData = [
+    {
+        title: "Distinctively Exploit Optimal Alignments with Mentality Networks",
+        abstract: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        slug: "1",
+        content: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        thumb: "small-5.jpg",
+        badge: "NEW",
+        type: "image"
+    },
+    {
+        title: "Progressively Maintain Extensive Infomediaries",
+        abstract: "Credibly reintermediate backend ideas for cross-platform models. Continually reintermediate integrated processes through technically sound intellectual capital.",
+        slug: "2",
+        content: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        thumb: "small-2.jpg",
+        badge: "UPDATE",
+        type: "video"
+    },
+    {
+        title: "Assertively Iterate Resource Maximizing",
+        abstract: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.",
+        slug: "3",
+        content: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        thumb: "small-3.jpg",
+        type: "image"
+    },
+    {
+        title: "Podcasting Operational Change Management Inside of Workflows",
+        abstract: "Quickly deploy strategic networks with compelling e-business. Credibly pontificate highly efficient manufactured products and enabled data.",
+        slug: "4",
+        content: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        thumb: "small-7.jpg",
+        badge: "TRENDING",
+        type: "image"
+    },
+    {
+        title: "Objectively Innovate Empowered Manufactured Products",
+        abstract: "Completely synergize resource taxing relationships via premier niche markets.",
+        slug: "5",
+        content: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        thumb: "small-1.jpg",
+        type: "image"
+    },
+    {
+        title: "Completely Pursue Scalable Customer Service",
+        abstract: "Credibly reintermediate backend ideas for cross-platform models. Continually reintermediate integrated processes through technically sound intellectual capital.",
+        slug: "6",
+        content: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        thumb: "small-6.jpg",
+        type: "image"
+    },
+    {
+        title: "Dynamically Procrastinate B2C Users After Installed Base Benefits",
+        abstract: "Dramatically maintain clicks-and-mortar solutions without functional solutions. Completely synergize resource taxing relationships via premier niche markets.",
+        slug: "7",
+        content: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        thumb: "small-7.jpg",
+        type: "image"
+    },
+    {
+        title: "Progressively Maintain Extensive Infomediaries",
+        abstract: "Dramatically maintain clicks-and-mortar solutions without functional solutions.",
+        slug: "8",
+        content: "Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks. \n\
+        Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate via plug-and-play networks.",
+        thumb: "small-8.jpg",
+        type: "image"
+    }
+]
