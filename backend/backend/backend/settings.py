56e64fb93e4a853d6847e4494cb1c273c07bec56
Fereydoon <ferydoon.jafari@gmail.com>
2022-07-13 13:15:05 +0430

    feat: Add cors to project

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'drf_yasg',
     'djoser',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -136,3 +138,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
