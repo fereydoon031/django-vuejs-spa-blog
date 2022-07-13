9e3d1ccf9aefb7e2f9be57e00d9940a6201826f7
Fereydoon <ferydoon.jafari@gmail.com>
2022-07-13 16:42:48 +0430

    feat: Add django logout to vuejs

--- src/views/LogoutView.vue
+++ src/views/LogoutView.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="LogOut">
     <h1>This is an login page</h1>
+    <div class="alert alert-danger"  v-if="logoutError">
+      {{this.logoutError}}
+    </div>
 <button class="btn btn-danger" @click="doLogout">Logout</button>
 
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'LogOut',
+  data(){
+    return{
+      logoutError: ''
+    }
+  },
   methods:{
     doLogout(){
+      axios
+        .post('/api/auth/token/logout/')
+        .then(response => {
+          this.$store.commit('logout',response.data.auth_token)
+          this.$router.push("/login")
+          })
+        .catch(error=>{ console.log(error)
+          
+          if (error.response) {
+            console.log(error.response.data)
+            this.logoutError = error.response.data
+          } else if (error.request) {
+
+            console.log(error.request);
+          } else {
+            // Some other error
+            console.log('Error', error.message);
+          }
+          })
+
       this.$store.commit("logout" )
       this.$router.push("/login")
     }
