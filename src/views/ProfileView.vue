704854d650164950dece65abb74d48769fd9479d
Fereydoon <ferydoon.jafari@gmail.com>
2022-07-14 15:28:24 +0430

    feat: Profile page connect to backend and page style be modified

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,259 @@
 <template>
-  <div class="Profile">
-    <h1>This is a profile page</h1>
-    <div class="alert alert-success"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, optio aperiam, quaerat aliquam sunt illum doloribus assumenda magni laboriosam, tenetur ea eos asperiores officia. Maxime magnam quo alias perspiciatis non!</div>
-  </div>
+    <div class="container py-1">
+<section class="section about-section gray-bg" id="about">
+            <div class="container">
+                <div class="row align-items-center flex-row-reverse">
+                    <div class="col-lg-6">
+                        <div class="about-text go-to">
+                            <h3 class="dark-color">About Me</h3>
+                            <h6 class="theme-color lead">A Lead Full Stack developer based in Tehran</h6>
+                            <p class="text-justify">I’m currently interested in working on robotics, robot operation system (ROS),
+                               autonomous vehicles, game development, artificial intelligence
+                                <mark>also design and develop services for customers of all siz.</mark>
+                                <br>Just enjoy the language code and dive deep and learn the basics. </p>
+                            <div class="row about-list">
+                                <div class="col-md-6">
+                                    <div class="media">
+                                        <label>Birthday</label>
+                                        <p>4th april 1998</p>
+                                    </div>
+                                    <div class="media">
+                                        <label>Age</label>
+                                        <p>22 Yr</p>
+                                    </div>
+                                    <div class="media">
+                                        <label>Residence</label>
+                                        <p>Canada</p>
+                                    </div>
+                                    <div class="media">
+                                        <label>Address</label>
+                                        <p>Tehran, IRAN</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="media">
+                                        <label>E-mail</label>
+                                        <p>info@domain.com</p>
+                                    </div>
+                                    <div class="media">
+                                        <label>Phone</label>
+                                        <p>+98-21-</p>
+                                    </div>
+                                    <div class="media">
+                                        <label>Skype</label>
+                                        <p>skype.id</p>
+                                    </div>
+                                    <div class="media">
+                                        <label>Freelance</label>
+                                        <p>Available</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class=" mb-4">
+                        <div class="text-center">
+                          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+                            class="rounded-circle img-fluid" style="width: 150px;">
+                          <h5 class="my-3">{{username}}</h5>
+                          <p class="text-muted mb-1">Full Stack Developer</p>
+                          <p class="text-muted mb-4">Tehran, IRAN </p>
+                          <div class="d-flex justify-content-center mb-1">
+                            <button type="button" class="btn btn-primary  mr-1">Follow</button>
+                            <button type="button" class="btn btn-outline-primary ms-1">Message</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <div class="counter">
+                    <div class="row">
+                        <div class="col-6 col-lg-3">
+                            <div class="count-data text-center">
+                                <h6 class="count h2" data-to="500" data-speed="500">500</h6>
+                                <p class="m-0px font-w-600">Happy Clients</p>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3">
+                            <div class="count-data text-center">
+                                <h6 class="count h2" data-to="150" data-speed="150">150</h6>
+                                <p class="m-0px font-w-600">Project Completed</p>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3">
+                            <div class="count-data text-center">
+                                <h6 class="count h2" data-to="850" data-speed="850">850</h6>
+                                <p class="m-0px font-w-600">Photo Capture</p>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3">
+                            <div class="count-data text-center">
+                                <h6 class="count h2" data-to="190" data-speed="190">190</h6>
+                                <p class="m-0px font-w-600">Telephonic Talk</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'ProfileView',
+    data(){
+      return{
+        username: '',
+      }
+    },
+    mounted(){
+      axios
+        .get('/api/auth/users/me/')
+        .then(response => {
+            console.log(response.data)
+            this.username = response.data.username
+        })
+        .catch(error=>{ console.log(error)
+          if (error.response) {
+            console.log(error.response)   
+            this.$store.commit("logout" )
+            this.$router.push("/login")
+          } else if (error.request) {
+            console.log(error.request)
+          } else {            
+            console.log('Error', error.message)
+          }
+        })
+    }
+  }
+</script>
+
+<style scoped>
+body{
+    color: #6F8BA4;
+    margin-top:20px;
+}
+.section {
+    padding: 100px 0;
+    position: relative;
+}
+.gray-bg {
+    background-color: #f5f5f5;
+}
+img {
+    max-width: 100%;
+}
+img {
+    vertical-align: middle;
+    border-style: none;
+}
+/* About Me 
+---------------------*/
+.about-text h3 {
+  font-size: 45px;
+  font-weight: 700;
+  margin: 0 0 6px;
+}
+@media (max-width: 767px) {
+  .about-text h3 {
+    font-size: 35px;
+  }
+}
+.about-text h6 {
+  font-weight: 600;
+  margin-bottom: 15px;
+}
+@media (max-width: 767px) {
+  .about-text h6 {
+    font-size: 18px;
+  }
+}
+.about-text p {
+  font-size: 18px;
+}
+.about-text p mark {
+  font-weight: 600;
+  color: #20247b;
+}
+
+.about-list {
+  padding-top: 10px;
+}
+.about-list .media {
+  padding: 5px 0;
+}
+.about-list label {
+  color: #20247b;
+  font-weight: 600;
+  width: 88px;
+  margin: 0;
+  position: relative;
+}
+.about-list label:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 11px;
+  width: 1px;
+  height: 12px;
+  background: #20247b;
+  -moz-transform: rotate(15deg);
+  -o-transform: rotate(15deg);
+  -ms-transform: rotate(15deg);
+  -webkit-transform: rotate(15deg);
+  transform: rotate(15deg);
+  margin: auto;
+  opacity: 0.5;
+}
+.about-list p {
+  margin: 0;
+  font-size: 15px;
+}
+
+@media (max-width: 991px) {
+  .about-avatar {
+    margin-top: 30px;
+  }
+}
+
+.about-section .counter {
+  padding: 22px 20px;
+  background: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 0 30px rgba(31, 45, 61, 0.125);
+}
+.about-section .counter .count-data {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.about-section .counter .count {
+  font-weight: 700;
+  color: #20247b;
+  margin: 0 0 5px;
+}
+.about-section .counter p {
+  font-weight: 600;
+  margin: 0;
+}
+mark {
+    background-image: linear-gradient(rgba(252, 83, 86, 0.6), rgba(252, 83, 86, 0.6));
+    background-size: 100% 3px;
+    background-repeat: no-repeat;
+    background-position: 0 bottom;
+    background-color: transparent;
+    padding: 0;
+    color: currentColor;
+}
+.theme-color {
+    color: #fc5356;
+}
+.dark-color {
+    color: #20247b;
+}
+
+</style>
